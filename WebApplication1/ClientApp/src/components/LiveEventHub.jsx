5b772c55a2f496e3f7866614a7f31b91d1a42951
G0PA <charger92@abv.bg>
2019-06-14 00:27:33 +0300

    live event hub for front end

---
+++ WebApplication1/ClientApp/src/components/LiveEventHub.jsx
@@ -0,0 +1,38 @@
+﻿import React, { Component } from 'react';
+import { HubConnection } from '@aspnet/signalr-client';
+
+export class LiveEventHub extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            nick: '',
+            message: '',
+            messages: [],
+            hubConnection: null,
+        };
+    }
+
+    componentDidMount = () => {
+        const nick = window.prompt('Your name:', 'John');
+
+        const hubConnection = new HubConnection('/LiveEvents');
+
+        this.setState({ hubConnection, nick }, () => {
+            this.state.hubConnection
+                .start()
+                .then(() => console.log('Connection started!'))
+                .catch(err => console.log('Error while establishing connection :('));
+
+            this.state.hubConnection.on('sendToAll', (nick, receivedMessage) => {
+                const text = `${nick}: ${receivedMessage}`;
+                const messages = this.state.messages.concat([text]);
+                this.setState({ message: messages });
+            });
+        });
+    }
+
+    render() {
+        return <div>{this.state.message}</div>;
+    }
+}
