0054eadaab722b52932e2d51b6adc54e0676e55f
unknown <charger92@abv.bg>
2019-07-21 23:04:17 +0300

    live games main page initial visualization

--- WebApplication1/ClientApp/src/components/LiveGame.jsx
+++ WebApplication1/ClientApp/src/components/LiveGame.jsx
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
-
+import { Button } from 'react-bootstrap';
+import { Random } from 'react-animated-text';
 export class LiveGame extends Component {
     constructor(props) {
         super(props);
@@ -22,9 +23,19 @@ export class LiveGame extends Component {
     render() {
         let body = [];
         if (this.state.liveEvent) {
-            body = <h1>{this.state.liveEvent.homeTeam.teamName} vs {this.state.liveEvent.awayTeam.teamName} </h1>;
-        }
-        return (
+            body = <div>
+                <Random
+                    effect="verticalFadeOut"
+                    effectDirection="down"
+                    style={{ color: 'red' }}
+                    effectChange="3.0"
+                    text="LIVE!" />
+                <Button bsStyle="primary" onClick={this.show}>
+                {this.state.liveEvent.homeTeam.teamName} vs {this.state.liveEvent.awayTeam.teamName}
+                </Button>
+</div>        
+                }
+                return (
             <div>
                 {body}
             </div>
